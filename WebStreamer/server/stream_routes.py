7495d40786254e106fa906db2db21b3a48a524df
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-03-11 07:15:39 +0530

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -1,5 +1,4 @@
-# Taken from megadlbot_oss <https://github.com/eyaadh/megadlbot_oss/blob/master/mega/webserver/routes.py>
-# Thanks to Eyaadh <https://github.com/eyaadh>
+# (c) Star Bots Tamil
 
 import re
 import time
