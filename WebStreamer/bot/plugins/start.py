bce08565d961e09365cd37339b495ca0f8a51120
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-03-14 06:37:01 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -279,31 +279,3 @@ async def sts(c, m):
         quote=True
     )
 
-################################################################################################################################################################################################################################################
-# Telegram Files List
-
-FILES_LIST_BUTTONS = [
-            [
-                InlineKeyboardButton('👨🏻‍💻 Creator', url='https://t.me/Star_Movies_Karthik'),
-                InlineKeyboardButton('📢 Update Channel', url='https://t.me/Star_Moviess_Tamil')
-            ]
-        ]
-
-@StreamBot.on_message(files_list_filter)
-async def files_list(client, message):
-    mention = message.from_user.mention
-    reply_markup = InlineKeyboardMarkup(ABOUT_BUTTONS)
-    if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
-        return await m.reply(
-            "<b>Hello 👋🏻 {mention} ❤️,\nSorry {mention}! You're not the Subscriber of Our Premium Plans, Only Subscribers of Our Premium Plans Can Use Our [File to Link Star Bots](https://t.me/File_to_Link_Star_Bot).</b>",
-            disable_web_page_preview=True, quote=True
-        )
-    await message.reply_text(
-            text="<b>Hi 👋🏻 {} ♥️, Telegram Files List</b>".format(
-                mention
-            ),
-            quote=True,
-            parse_mode=ParseMode.HTML,
-            reply_markup=reply_markup,
-            disable_web_page_preview=True
-        )
