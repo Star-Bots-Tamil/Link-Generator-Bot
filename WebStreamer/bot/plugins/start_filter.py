5cd7b3c9aa066b9ed60a54f5770ef0531b0e1637
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-03-16 07:16:34 +0530

    Delete start_filter.py

--- WebStreamer/bot/plugins/start_filter.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import filters
-
-def s_filter(_,__,message):
-   if message.text == "/start":
-      return True
-   else:
-      return False
-
-start_filter = filters.create(s_filter)
