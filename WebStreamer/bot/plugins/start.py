09ae97a324bccceaa7152d46025e83005e100fa4
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-14 16:55:31 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -64,7 +64,7 @@ async def _(bot, cmd):
         else:
             logging.info(f"New User :- Name :- {message.from_user.first_name} ID :- {message.from_user.id}")
 
-@StreamBot.on_message(filters.command("start") & filters.private & filters.incoming)
+@StreamBot.on_message(filters.command("start"))
 async def start(client, m: Message):
     reply_markup = InlineKeyboardMarkup(MAIN_MENU_BUTTONS)
     mention = m.from_user.mention(style="md")
